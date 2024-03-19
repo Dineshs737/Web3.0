26748372379880d3dd040e7c35d2e89768a1ea4e
Dineshs737 <dinesh77saarck@gmail.com>
2024-03-19 21:48:19 +0530

    update

--- BICT/js/loops/loop1/script.js
+++ BICT/js/loops/loop1/script.js
@@ -12,10 +12,47 @@
 //     console.log(i)
 // }
 
-while(true)
-{
-    let num = Number(prompt("Enter the number :"))
-    if(!isNaN(num))
-        break;
+// while(true)
+// {
+//     let num = Number(prompt("Enter the number :"))
+//     if(!isNaN(num))
+//         break
 
+// }
+
+console.log("continue demi")
+
+for(let i=1 ; i<=10 ; i++)
+{
+    if(i%3==0) continue
+    console.log(i)
+}
+
+console.log("using array values in java script")
+
+ arr = ['apple','orange','grapes','mango']
+
+console.log(arr)
+
+// get value from array using for loop
+
+for( i=0 ;i<arr.length ; i++)
+{
+    console.log(arr[i].toUpperCase())
+}
+console.log("\n")
+for(let fruit of arr)   console.log(fruit)
+
+item ={
+    name: "phone",
+    quantity:1,
+    price:25000
+}
+
+console.log("\n")
+//access object using for looop
+
+for(let key in item)
+{
+    console.log(item[key])
 }
